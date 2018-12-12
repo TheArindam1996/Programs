2a5a0dbe2c162416c21ab1b275e41749ccdbfb14
TheArindam1996 <45827530+TheArindam1996@users.noreply.github.com>
2018-12-12 22:19:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        string EmployeeName;
+        int EmployeeId;
+        double Salary;
+        public string EmpName
+        {
+            set
+            {
+                EmployeeName = value;
+            }
+            get
+            {
+                return EmployeeName;
+            }
+        }
+        public int EMPId
+        {
+            set
+            {
+                EmployeeId = value;
+            }
+            get
+            {
+                return EmployeeId;
+            }
+        } 
+        public double EmpSalary
+        {
+            set
+            {
+                Salary = value;
+            }
+            get
+            {
+                return Salary;
+            }
+        }
+    }
+}
